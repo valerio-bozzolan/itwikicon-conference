18e5410a1d39d47f3c7c5ff34c644c986eaad743
Valerio Bozzolan <boz@reyboz.it>
2022-07-11 11:52:56 +0200

    2022: update home info

--- 2022/www/index.php
+++ 2022/www/index.php
@@ -50,11 +50,15 @@ template_2022( 'header', [
 				'<b>' . esc_html( $conference->getConferenceTitle() ) . '</b>',
 				esc_html( $conference->getConferenceSubtitle() )
 			) ?></p>
-				<p class="flow-text"><?= esc_html( __( "Quest'anno l'evento si terrà di persona a Verbania!" ) ) ?></p>
+				<p class="flow-text"><?= esc_html( __( "Finalmente l'evento si terrà nuovamente di persona… a Verbania!" ) ) ?></p>
 			<p><a href="#step-1" class="btn-large waves-effect blue smooth-scroll"><?= icon_2022( 'info', 'left' ) ?> <?= __( "Info" ) ?></a></p>
 		</div>
 	</div>
 </div>
+
+<div id="step-1"></div>
+<h2 class="blue-text center"><b>Come partecipare</b> dal vivo alla itWikiCon 2022?</h2>
+<p class="center">Segui tutti i passi per ogni partecipante.<br /><em><small><?= sprintf( __( "Tempo richiesto: %s minuti" ), '10-15' ) ?></small></em></p>
 
 <div class="container" id="info">
 
@@ -62,11 +66,10 @@ template_2022( 'header', [
 	<div class="row">
 
 	<div class="col s12">
-		<div id="step-1"></div>
-		<h2 class="blue-text">Come partecipare alla itWikiCon 2022?</h2>
 		<div class="card-panel">
 			<h3 class="blue-text">Passo 1 - Registrazione</h3>
-			<p>Se desideri partecipare di persona, compila il modulo di registrazione:<br />
+			<p>Per favore compila il modulo di registrazione individuale:<br />
+				<em><small><?= sprintf( __( "Tempo richiesto: %d minuti" ), 8 ) ?></small></em><br />
 				<a class="btn-large blue waves-effect" href="https://survey.itwikicon.org/index.php/394377" rel="noopener noreferrer nofollow"><?=
 					icon_2022( 'group_add', 'left' )
 					.
@@ -80,11 +83,15 @@ template_2022( 'header', [
 		<div id="step-2"></div>
 		<div class="card-panel">
 			<h3 class="blue-text">Passo 2 - Pagamento</h3>
-			<p>Se desideri partecipare di persona, paga la quota di partecipazione:<br />
-				<a class="btn-large blue waves-effect" href="https://www.paypal.com/donate/?hosted_button_id=35MC3WB2QRP7L" target="_blank"><?=
+			<p>Per favore aiuta questo evento organizzato dalla community, pagando la quota di partecipazione che copre pasti catering ecc.<br />
+			Questa spesa ti sarà rimborsata se richiederai la borsa di partecipazione e se non saranno terminate.<br />
+			Apri la pagina e clicca su <em>Contribuisci</em>:<br />
+			<br />Non usiamo PayPal ma Splitted che ha sede in provincia di Milano. È semplice da usare.<br />
+				<em><small><?= sprintf( __( "Tempo richiesto: %d minuti" ), 2 ) ?></small></em><br />
+				<a class="btn-large blue waves-effect" href="https://splitted.it/itwikicon-2022/" target="_blank"><?=
 					icon_2022( 'payment', 'left' )
 					.
-					__( "Pagamento PayPal" )
+					__( "Pagamento partecipazione" )
 				?></a>
 			</p>
 		</div>
@@ -93,12 +100,14 @@ template_2022( 'header', [
 	<div class="col s12">
 		<div id="step-3"></div>
 		<div class="card-panel">
-			<h3 class="blue-text">Passo 3 - Viaggio e preferenze alimentari</h3>
-			<p>Compilare il questionario anonimo su viaggio e preferenze alimentari:<br />
+			<h3 class="blue-text">Passo 3 - Viaggio ed esigenze alimentari</h3>
+			<p>Compilare il questionario anonimo su viaggio e le tue esigenze alimentari.<br />
+			<p>Notare che è separato così questi dati sensibili rimangono anonimi.<br />
+				<small><?= sprintf( __( "Tempo richiesto: %d minuti" ), 2 ) ?></small><br />
 				<a class="btn-large blue waves-effect" href="https://survey.itwikicon.org/index.php/471745"><?=
 					icon_2022( 'train', 'left' )
 					.
-					__( "Viaggio e pref. alimentari" )
+					__( "Viaggio ed esigenze alimentari" )
 				?></a>
 			</p>
 		</div>
@@ -107,10 +116,22 @@ template_2022( 'header', [
 	<div class="col s12">
 		<div id="step-4"></div>
 		<div class="card-panel">
-			<h3 class="blue-text">Passo 4 - Informazioni</h3>
-			<p>Ti invitiamo inoltre ad iscriverti al canale Telegram itWikiCon (<a href="https://t.me/itwikicon"><span style="color: #000080;"><span style="color: #0000ff;">https://t.me/itwikicon</span></span></a>) per essere sempre aggiornato in tempo reale su eventuali modifiche al programma e per ricevere altre informazioni utili.</p>
-			<p>Per qualsiasi domanda puoi scrivere su meta: <span style="color: #0000ff;"><a style="color: #0000ff;" href="https://meta.wikimedia.org/wiki/Talk:ItWikiCon/2022/Partecipanti">https://meta.wikimedia.org/wiki/Talk:ItWikiCon/2022/Partecipanti</a></span> oppure via e-mail a: info@itwikicon.org</p>
-			<p>Ci vediamo a Verbania! ❤️</p>
+			<h3 class="blue-text">Fine! Ci vediamo a Verbania ❤️</h3>
+			<p>Strumenti utili per rimanere informati fino ad allora:</p>
+			<ul class="collection">
+				<li class="collection-item">
+					Per conoscere e organizzarsi con gli altri partecipanti su meta-wiki:<br />
+					<a class="blue-text" href="https://meta.wikimedia.org/wiki/ItWikiCon/2022/Partecipanti"><?= icon_2022( 'group', 'left' ) ?> pagina dei partecipanti itWikiCon 2022 su Meta-wiki</a>
+				</li>
+				<li class="collection-item">
+					Per stare in chat con organizzatori e partecipanti e ricevere in tempo reale<br />(consigliamo di silenziare le notifiche - così si chatta solo quando si è in chat, ma si ricevono comunque gli annunci fissati in alto)<br />
+					<a class="blue-text" href="https://t.me/itwikicon"><?= icon_2022( 'chat', 'left' ) ?>gruppo Telegram @itwikicon</a>
+				</li>
+				<li class="collection-item">
+					Per inoltrare una domanda personale:<br />
+					<a href="mailto:info@itwikicon.org"><?= icon_2022( 'mail', 'left' ) ?> info@itwikicon.org</a>
+				</li>
+			</ul>
 		</div>
 	</div>
 
